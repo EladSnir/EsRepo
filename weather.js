727af6653fd85ab03295383b10bcb2290387916f
EladSnir <eladsnir@gmail.com>
2019-06-16 18:54:25 +0300

    first commit

---
+++ weather.js
@@ -0,0 +1,13 @@
+// const url = 'http://api.openweathermap.org/data/2.5/weather?q=jerusalem,israel&units=metric&appid=1e8ffdb64b7d4d8dbdd4bf3a700bdeb0';
+const url = 'weather.json';
+function reqListener () {
+    var obj = JSON.parse(this.responseText)
+    console.log(obj);
+  }
+  
+  var oReq = new XMLHttpRequest();
+  oReq.addEventListener("load", reqListener);
+  oReq.open("GET", url);
+  oReq.send();
+
+  // 1e8ffdb64b7d4d8dbdd4bf3a700bdeb0//
